b2e05f14cd6b54ab7fb040ea4c50092130447c7c
huangweiqi <askviky2010@gmail.com>
2018-06-20 10:15:47 +0800

    添加jni性能测试代码

---
+++ app/src/main/java/com/abt/jni/performance/JniWrapper.java
@@ -0,0 +1,19 @@
+package com.abt.jni.performance;
+
+/**
+ * Created by ytr on 17-4-8.
+ * <p>
+ * We use floating-point operations to determine the performance of Jni,
+ * this is a time-consuming operation in the computer.
+ */
+public class JniWrapper {
+
+    static {
+        System.loadLibrary("jni_pref");
+    }
+
+    /**
+     * mock a time-consuming task.
+     */
+    native public static double calculationTask(int loopCount);
+}
